821e10068730ea73d57b02f9dde058217d406e04
SoumenG <soumen.xplorin@gmail.com>
2013-08-11 00:57:40 +0530

    Added Project

---
+++ dist/bnWordnet-1.0pre1dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name ='bnWordnet',
+version ='1.0pre1dev',
+description ='Bengali Wordnet',
+author ='Soumen Ganguly',
+author_email ='soumendotganguly@gmail.com',
+packages =['bnwordnet','bnwordnet.test'],
+)
